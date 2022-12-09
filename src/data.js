6147569d2399d67bebafb5af1f9be56740994591
Te Su <sute515@gmail.com>
2022-12-08 21:32:53 -0800

    Add master copy

---
+++ src/data.js
@@ -0,0 +1,191 @@
+const data = [{
+    quote: "I'll bring the luck with me.",
+    source: "The Old Man and the Sea, Ernest Hemingway"
+},
+{ 
+    quote: "Luck is a thing that comes in many forms and who can recognize her?",
+    source: "The Old Man and the Sea, Ernest Hemingway"
+},
+{
+    quote: "The most circumspect and most timid people at times dare to do the most extreme things.",
+    source: "Either/Or, Kierkegaard"
+},
+{
+    quote: "To lie is not only to say what is not the case. It also, above all, means saying more than is the case, and, as far as the human heart is concerned, more than we feel. It is what we all do, every day, to simplify life.",
+    source: "Camus on The Outsider, Albert Camus"
+}, 
+{
+    quote: "If we are to understand the world, we must turn aside from it; if we are to serve men better, we must briefly hold them at a distance.",
+    source: "The Minotaur or The Stop In Oran, Albert Camus"
+},
+{
+    quote: "People rarely understand that it is never through despair that a man gives up what constituted his life. Impulses and moments of despair lead towards other lives, and merely indicate a quivering attachment to the lessons of the earth. But it can happen when he reaches a certain degree of lucidity a man feels his heart is closed, and without protest or rebellion turns his back on what up to then he had taken for his life, that is to say his restlessness.",
+    source: "The Desert, Albert Camus"
+},
+{
+    quote: "If it is true that every truth bears its bitterness within it, it is also true that every denial contains a flourish of affirmations.",
+    source: "The Desert, Albert Camus"
+},
+{
+    quote: "You don’t get to choose if you get hurt in this world… but you do have some say in who hurts you.",
+    source: "The Fault In Our Stars, John Green"
+},
+{
+    quote: "Funerals, I had decided, are for the living.",
+    source: "The Fault In Our Stars, John Green"
+},
+{
+    quote: "Some tourists think Amsterdam is a city of sin, but in truth it is a city of freedom. And in freedom, most people find sin.",
+    source: "The Fault In Our Stars, John Green"
+},
+{
+    quote: "The weird thing about houses is that they almost always look like nothing is happening inside of them, even though they contain most of our lives.",
+    source: "The Fault In Our Stars, John Green"
+},
+{
+    quote: "You put the killing thing right between your teeth, but you don’t give it the power to do its killing.",
+    source: "The Fault In Our Stars, John Green"
+},
+{
+    quote: "People like you to be something, preferably what they are.",
+    source: "East of Eden, John Steinbeck"
+},
+{
+    quote: "He could hear the rich, lyric voice in his ears, the tones rising and falling in their foreignness, and the curious music of oddly chosen words tripping out so that you were never sure what the next word would be. In the speech of most men you are absolutely sure what the next word will be.",
+    source: "East of Eden, John Steinbeck"
+},
+{
+    quote: "Some people think it’s an insult to the glory of their sickness to get well. But the time poultice is no respecter of glories. Everyone gets well if he waits around.",
+    source: "East of Eden, John Steinbeck"
+},
+{
+    quote: "It would be absurd if we did not understand both angels and devils, since we invented them.",
+    source: "East of Eden, John Steinbeck"
+},
+{
+    quote: "And it never failed that during the dry years the people forgot about the rich years, and during the wet years they lost all memory of the dry years. It was always that way.",
+    source: "East of Eden, John Steinbeck"
+},
+{
+    quote: "… your one duty is to live and be happy.",
+    source: "A Happy Death, Albert Camus"
+},
+{
+    quote: "To two men living the same number of years, the world always provides the same sum of experiences. It is up to us to be conscious of them. Being aware of one’s life, one’s revolt, one’s freedom, and to the maximum, is living. Where lucidity dominates, the scale of values becomes useless.",
+    source: "The Myth of Sisyphus, Albert Camus"
+},
+{
+    quote: "According to Nietzsche, when we understand that life is a battle that is worth putting all one’s energy into, a battle that is worth fighting and winning, then victory and sleep will be our rewards, for no matter what happens, we will live life fully, to exhaustion, and therefore will sleep peacefully and have created a work of art worth repeating endlessly.",
+    source: "Everyday Mysteries, Emmy van Deurzen-Smith"
+},
+{
+    quote: "… every reason can be met with another one…",
+    source: "The Fall, Albert Camus"
+},
+{
+    quote: "People hasten to judge in order not to be judged themselves.",
+    source: "The Fall, Albert Camus"
+}, 
+{
+    quote: "When people change direction it is a rare one who does not spend the first half of his journey looking back over his shoulder.",
+    source: "John Steinbeck, Sweet Thursday"
+},
+{
+    quote: "Don’t you never try to run away from nothing, because you can’t.",
+    source: "John Steinbeck, Sweet Thursday"
+},
+{
+    quote: "You can only fight Fate so far, and when you give in to it you’re very strong; because all of your force flows in one direction.",
+    source: "John Steinbeck, Sweet Thursday"
+},
+{
+    quote: "Classification of fantasy and reality is itself a fantasy that humans created… Life begins when you make a distinction between yourself and others. From that moment on, the world becomes a stage for the story in which you are the main character. All humans live in a fantasy in which they are the main character. But the world doesn’t recognize you as the main character at all. What nonsense. Everyone lives their entire life tormented by this confusion. There’s only one way out of this hell. To place yourself in a postion that is neither the main character nor a supporting role. In other words, the Author.",
+    source: "Kino’s Journey"
+},
+{
+    quote: "The redwoods, once seen, leave a mark or create a vision that stays with you always… From them comes silence and awe. It’s not only their unbelievable stature, nor the color which seems to shift and vary under your eyes, no, they are not like any tree we know, they are ambassadors from another time… The vainest, most slap-happy and irreverent of men, in the presence of redwoods, goes under a spell of wonder and respect.",
+    source: "John Steinbeck"
+},
+{
+    quote: "The people say that the two seemed to be removed from human experience; that they had gone through pain and had come out on the other side; that there was almost a magical protection about them.",
+    source: "John Steinbeck, The Pearl"
+},
+{
+    quote: "You have defied not the pearl buyers, but the whole structure, the whole way of life.",
+    source: "John Steinbeck, The Pearl"
+},
+{
+    quote: "…even though these old people are no longer useful in any traditional meaning of the word, they are there to be loved unconditionally, like trees in the winter.",
+    source: "Bird by Bird, Anne Lamott"
+},
+{
+    quote: "You can’t save up your life and live it later.",
+    source: "Unknown"
+},
+{
+    quote: "In the depth of winter, I finally learned that there was in me an invincible summer.",
+    source: "Albert Camus"
+},
+{
+    quote: "I learned very early the difference between knowing the name of something and knowing something.",
+    source: "Richard Feynman"
+},
+{
+    quote: "If I had more time I would write a shorter letter.",
+    source: "Blaise Pascal"
+},
+{
+    quote: "The landscape one admires so much as a picture generally makes for an uncomfortable bed.",
+    source: "The Book of Disquiet, Fernando Pessoa"
+},
+{
+    quote: "One gets to grow at whatever level that they want to grow at.",
+    source: "Michael Singer"
+},
+{
+    quote: "Personality is what you came up with to live with yourself.",
+    source: "Michael Singer"
+},
+{
+    quote: "Is it possible to be so completely yourself that the mind-made image of me dissolves?",
+    source: "Eckhart Tolle"
+},
+{
+    quote: "Meursault, contrary to appearances, does not want to simplify life. He says what he is, he refuses to exaggerate his feelings, and immediately society feels itself threatened… Meursault, for me, is thus not a piece of social wreckage, but a man who is naked and poor, in love with the sun that leaves no shadows. Far form being empty of all feelings, he is inspired by a passion which is deep because it is stubborn, a passion for the absolute and for truth. The truth is still a negative one, the truth of what we are and what we feel, but without it no conquest of ourselves or of the world will ever be possible.",
+    source: "Camus on The Outsider, Albert Camus"
+}, {
+    quote: "A journey of a thousand miles begins with a single step.",
+    source: "Lao Tzu"
+}, {
+    quote: "The flame that burns Twice as bright burns half as long.",
+    source: "Lao Tzu"
+}, {
+    quote: "Nature does not hurry, yet everything is accomplished.",
+    source: "Lao Tzu"
+}, {
+    quote: "To attain knowledge, add things everyday. To attain wisdom, remove things every day.",
+    source: "Lao Tzu"
+}, {
+    quote: "How in the world did a person get to be where I was?",
+    source: "The Poisonwood Bible, Barbara Kingsolver"
+}, {
+    quote: "And the rain poured down and I wondered, Are we lost right now without knowing it?",
+    source: "The Poisonwood Bible, Barbara Kingsolver"
+}, {
+    quote: "The world is constantly changing. And you can't expect it to stay the same, even for a second. If that's true, there's no need for you to go on a journey, Kino. Even if you stay in one place, you'll be able to keep experiencing the world!",
+    source: "Kino's Journey"
+}, {
+    quote: "It is fun to be in the same decade with you.",
+    source: "Kino's Journey"
+}, {
+    quote: "The snow falls, each flake in its appropriate place.",
+    source: "Kino's Journey"
+}, {
+    quote: "Nobody realizes that some people expend tremendous energy merely to be normal.",
+    source: "Albert Camus"
+}, {
+    quote: "You don't have to catch the train. The train must stop in front of you. And if you don't like it wait for the next train that will also stop in front of you. Because you demand things or events not the opposite.",
+    source: "Unknown"
+}];
+
+module.exports = { data };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const { data } = require('./data.js');
+const { colors } = require('./colors.js');
+
+class Quotemachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      randomIndex: 0,
+      randomIndexColor: 0,
+    };
+    this.changeQuote = this.changeQuote.bind(this);
+  }
+  changeQuote() {
+    this.setState({
+      randomIndex: Math.floor(Math.random() * data.length),
+      randomIndexColor: Math.floor(Math.random() * colors.length),
+    });
+  }
+
+  render() {
+    let currentQuote = data[this.state.randomIndex];
+    let colorStyle = colors[this.state.randomIndexColor];
+
+    return (
+      <div className = "App">
+        <button className = "button" onClick = {this.changeQuote}>New Quote</button>
+        <br />
+        <h2 style = {{color: colorStyle}}>
+          {currentQuote.quote}
+        </h2>
+        <br />
+        <h3 style = {{color: colorStyle}}>
+          {currentQuote.source}
+        </h3>
+      </div>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Quotemachine />);
